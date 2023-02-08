c3717c9df03b1c2ce8689bab50b881a230c54083
Lara Katyuce <111613855+larakatyuce7@users.noreply.github.com>
2023-02-08 12:23:51 -0300

    Add files via upload

---
+++ Programa media mediana e soma.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double N1, N2, N3, N4, N5, soma, mediana, media;
+
+            Console.WriteLine("Escreva o primeiro numero");
+            N1 = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("Escreva o segundo numero");
+            N2 = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("Escreva o terceiro numero");
+            N3 = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("Escreva o quarto numero");
+            N4 = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("Escreva o quinto numero");
+            N5 = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+
+            mediana = N3;
+
+            soma = N1 + N2 + N3 + N4 + N5;
+
+            media = N1 + N2 + N3 + N4 + N5;
+
+            media = media / 5;
+
+            Console.WriteLine("MÉDIA" + media);
+            Console.WriteLine("Mediana" + mediana);
+            Console.WriteLine("Soma" + soma);
+            Console.ReadKey();
+        }
+    }
+}
